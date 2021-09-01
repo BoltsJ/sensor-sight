766ae25654e6d683eac729870ba9d8b5202aa26a
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2021-09-01 10:52:00 -0500

    Patch Token.isVisible to be gridspace aware.
    This only works on the lancer system, since it implements a method on
    Tokens to return a set of grid spaces.

--- sensor-sight.js
+++ sensor-sight.js
@@ -1,17 +1,40 @@
-'use strict';
+"use strict";
 
-import {libWrapper} from './module/shim.js';
+import { libWrapper } from "./module/shim.js";
 
-Hooks.once('setup', () => {
-  libWrapper.register('sensor-sight', 'SightLayer.prototype.testVisibility', function (w, point, {tolerance=2, object=null}={}) {
-    // default logic
-    let r = w.apply(this, [point, {tolerance, object}]);
-    // is this within sensor range of any controlled token?
-    let s = canvas.tokens.controlled.reduce((a, t) => {
-      return a || inSensorRange(t, point);
-    }, false);
-    return r || s;
-  });
+Hooks.once("init", () => {
+  libWrapper.register(
+    "sensor-sight",
+    "SightLayer.prototype.testVisibility",
+    function (w, point, { tolerance = 2, object = null } = {}) {
+      // default logic
+      let r = w.apply(this, [point, { tolerance, object }]);
+      // is this within sensor range of any controlled token?
+      let s = canvas.tokens.controlled.reduce((a, t) => {
+        return a || inSensorRange(t, point);
+      }, false);
+      return r || s;
+    }
+  );
+
+  libWrapper.register(
+    "sensor-sight",
+    "Token.prototype.isVisible",
+    function (w) {
+      const vis = w.apply(this);
+      const tolerance = Math.round(
+        Math.min(canvas.grid.grid.w, canvas.grid.grid.h) / 4
+      );
+      return (
+        vis ||
+        Array.from(this.getOccupiedSpaces()).reduce((a, p) => {
+          return (
+            a || canvas.sight.testVisibility(p, { tolerance, object: this })
+          );
+        }, false)
+      );
+    }
+  );
 });
 
 function inSensorRange(token, point) {
@@ -19,6 +42,8 @@ function inSensorRange(token, point) {
   const rays = Array.from(token.getOccupiedSpaces()).map(p => {
     return { ray: new Ray(p, point) };
   });
-  const min_d = Math.min(...canvas.grid.grid.measureDistances(rays, {gridSpaces: true }));
+  const min_d = Math.min(
+    ...canvas.grid.grid.measureDistances(rays, { gridSpaces: true })
+  );
   return sensors >= min_d;
 }
